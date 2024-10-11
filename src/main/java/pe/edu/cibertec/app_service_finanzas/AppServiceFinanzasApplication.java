9ac51bb85bb51c10ce19f8cc3a6676611ebb476a
AsumaToki04 <megamanzxu@hotmail.com>
2024-10-11 15:28:45 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_service_finanzas/AppServiceFinanzasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_service_finanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServiceFinanzasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServiceFinanzasApplication.class, args);
+	}
+
+}
