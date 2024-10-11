3cced020e75b7b86df56545a82dec72020d15e06
AsumaToki04 <megamanzxu@hotmail.com>
2024-10-11 16:06:06 -0500

    Dependencias, Clases Modificadas e Interfaces Creadas
    Clases Modificadas:
    AppServiceFinanzasApplication
    GestionPagoController
    
    Interfaces Creadas:
    ServiceDocenteClient

--- src/main/java/pe/edu/cibertec/app_service_finanzas/AppServiceFinanzasApplication.java
+++ src/main/java/pe/edu/cibertec/app_service_finanzas/AppServiceFinanzasApplication.java
@@ -2,7 +2,9 @@ package pe.edu.cibertec.app_service_finanzas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class AppServiceFinanzasApplication {
 
--- src/main/java/pe/edu/cibertec/app_service_finanzas/controller/GestionPagoController.java
+++ src/main/java/pe/edu/cibertec/app_service_finanzas/controller/GestionPagoController.java
@@ -1,13 +1,22 @@
 package pe.edu.cibertec.app_service_finanzas.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.app_service_finanzas.remoteservice.ServiceDocenteClient;
 
 @RestController
+@RequiredArgsConstructor
 public class GestionPagoController {
+    private final ServiceDocenteClient serviceDocenteClient;
 
     @GetMapping("/pago")
     public String obtenerPagos() {
         return "Respuesta del servicio de Finanzas-Pagos";
+    }
+
+    @GetMapping("/pago-docente")
+    public String obtenerPagosDocentes() {
+        return serviceDocenteClient.obtenerDocentes();
     }
 }
